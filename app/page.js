0bc7a4840ba8b0173c33e5e1161e7907d2b16ff0
Akash <akash07vvit@gmail.com>
2023-12-20 19:42:36 +0530

    dashboaed

--- app/page.js
+++ app/page.js
@@ -1,11 +1,25 @@
-import Link from 'next/link'
-import RootLayout from './layout'
-import GoToHomeBtn from '@/components/dashboard/GoToHomeBtn'
+"use client"
+
+
+
+import GoToHomeBtn from '@/components/dashboard/GoToHomeBtn';
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
 export default function Home() {
+
+
+  const router = useRouter();
+
+  // useEffect(() => {
+  //   router.push("/home")
+  // },[])
   return (
-      <main className="mt-20">
-        <GoToHomeBtn />
+      <main className="mt-20 ml-60">
+        <div className='h-10 w-full bg-black text-white flex items-center justify-center rounded-full '>
+          <GoToHomeBtn />
+        </div>
+        <h2 className='text-center mt-10'>Click above button</h2>
       </main>
   )
 }
